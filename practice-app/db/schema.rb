7455f26dadd0e97a4fe0c6ab434f3114860d2cf2
nguyenduong2612 <nguyenduong261298@gmail.com>
2019-10-08 01:33:09 +0700

    complete week 4

--- practice-app/db/schema.rb
+++ practice-app/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_22_053514) do
+ActiveRecord::Schema.define(version: 2019_10_07_182821) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,8 @@ ActiveRecord::Schema.define(version: 2019_09_22_053514) do
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
