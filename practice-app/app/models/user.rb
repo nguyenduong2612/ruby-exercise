cbf837c525709b37a4d193a09d1ff9c681eb9208
nguyenduong2612 <nguyenduong261298@gmail.com>
2019-10-16 20:07:09 +0700

    complete week 5

--- practice-app/app/models/user.rb
+++ practice-app/app/models/user.rb
@@ -8,4 +8,14 @@ class User < ApplicationRecord
                     uniqueness: { case_sensitive: false }
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
+
+  def self.from_omniauth(auth_hash)
+    user = find_or_create_by(uid: auth_hash['uid'], provider: auth_hash['provider'])
+    user.name = auth_hash['info']['name']
+    user.email = auth_hash['info']['email']
+    user.password = Devise.friendly_token[0,20]
+
+    user.save!
+    user
+  end
 end
